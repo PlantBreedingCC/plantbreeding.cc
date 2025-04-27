f7cfb71ddc398fc1a51f7054b517c498fa4fe50f
trife <twrife@clemson.edu>
2025-04-27 05:06:32 -0400

    add url redirect

--- _includes/search-providers/default/search-data.js
+++ _includes/search-providers/default/search-data.js
@@ -5,9 +5,14 @@ window.TEXT_SEARCH_DATA={
       {%- for _article in _collection.docs -%}
       {%- unless forloop.first -%},{%- endunless -%}
       {'title':{{ _article.title | jsonify }},
-      {%- include snippets/prepend-baseurl.html path=_article.url -%}
-      {%- assign _url = __return -%}
-      'url':{{ _url | jsonify }}}
+      'url': {%- if _article.pdf -%}
+                  {{ _article.pdf | jsonify }}
+               {%- else -%}
+                  {%- include snippets/prepend-baseurl.html path=_article.url -%}
+                  {%- assign _url = __return -%}
+                  {{ _url | jsonify }}
+               {%- endif -%}
+      }
       {%- endfor -%}
     ]
   {%- endfor -%}
--- _outreach/infographic-food-security.md
+++ _outreach/infographic-food-security.md
@@ -3,6 +3,7 @@ layout: article
 title: Food Security
 key: outreach-food-security
 cover: /assets/images/outreach/infographic-food-security.png
+pdf: assets/outreach/infographic-food-security.pdf
 ---
 
 <embed src="/assets/outreach/infographic-food-security.pdf" width="500" height="375" type="application/pdf">
